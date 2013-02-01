b94622fd92f9e035887a205e57d57d82047b29bb
liamhodges <liamhodges@me.com>
2013-02-01 20:24:21 +1100

    Updated sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,11 @@
-<h2>This is a heading</h2>
+<h2>Recent posts</h2>
+
+<ul>
+<?php
+	$args = array( 'numberposts' => '5' );
+	$recent_posts = wp_get_recent_posts( $args );
+	foreach( $recent_posts as $recent ){
+		echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+	}
+?>
+</ul>
