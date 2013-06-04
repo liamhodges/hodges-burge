7dd73bb98e225ead89d70ff4ef75412f9161721a
liamhodges <liamhodges@me.com>
2013-06-04 21:42:48 +1000

    Style update

--- footer.php
+++ footer.php
@@ -5,7 +5,6 @@
 				<div id="about-us">
 					<h3>About us</h3>
 					<p>Conveniently revolutionize out-of-the-box manufactured products before exceptional innovation. Synergistically iterate parallel potentialities after transparent schemas. Progressively reconceptualize standards compliant deliverables after cross-media core competencies.</p>
-					<p>Synergistically orchestrate intuitive methodologies via long-term high-impact infomediaries. Uniquely synergize interoperable results after excellent e-business. Continually facilitate market-driven architectures rather than tactical catalysts for change.</p>
 				</div>
 			</div>
 		</div>
