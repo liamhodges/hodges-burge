43b9e6fe3afc367c7657d30485409c91dbac766d
liamhodges <liamhodges@me.com>
2013-06-05 21:43:11 +1000

    Style update
    Mostly responsive stuff.

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-			<div id="footer-wrapper">
+<!--			<div id="footer-wrapper">
 				<div id="seal">
 				</div>
 				<div id="about-us">
@@ -7,7 +7,7 @@
 					<p>Conveniently revolutionize out-of-the-box manufactured products before exceptional innovation. Synergistically iterate parallel potentialities after transparent schemas. Progressively reconceptualize standards compliant deliverables after cross-media core competencies.</p>
 				</div>
 			</div>
-		</div>
+		</div>-->
 
 
 </div> <!--wrapper div ends-->
